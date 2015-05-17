ad60209b05591617116f328a1fe49727d0d8294f
Manu Cornet <manu@manucornet.net>
2015-05-16 21:39:50 -0700

    First working version.

--- t411downloader.js
+++ t411downloader.js
@@ -9,12 +9,37 @@ T411Downloader = function() {};
  * @param {chrome.windows.Tab} tab The tab from which the action was triggered.
  */
 T411Downloader.prototype.start = function(tab) {
-  console.log('Starting download');
   chrome.tabs.query({
     url: 'http://*.t411.io/*'}, this.onTabsAvailable.bind(this));
 };
 
 
 T411Downloader.prototype.onTabsAvailable = function(tabs) {
-  console.log('Got ' + tabs.length + ' relevant tabs!');
+  for (var tab, i = 0; tab = tabs[i]; i++) {
+    chrome.tabs.executeScript(tab.id, {
+      code: "var merciElements = document.getElementsByClassName('btn call');" +
+            "if (merciElements.length) { var merciBtn = merciElements[0]; merciBtn.click(); }" +
+            "var starElements = document.getElementsByClassName('ui-stars-star');" +
+            "if (starElements.length == 5) { starElements[4].click(); }" +
+            "var buttons = document.getElementsByClassName('btn');" +
+            "var urlToDownload;" +
+            "for (var btn, j = 0; btn = buttons[j]; j++) {" +
+            "  if (/.*charger.*/.test(btn.textContent)) {" +
+            "    if (btn.href) {" +
+            "      urlToDownload = btn.href;" +
+            "    }" +
+            "  }" +
+            "}" +
+            "'' + " + tab.id + " + '!#~' + urlToDownload"   // Last statement gets returned.
+    }, function(hrefArray) {
+      if (!!hrefArray && !!hrefArray[0]) {
+        var components = hrefArray[0].split('!#~', 2);
+        if (!!components[1] && components[1] != 'undefined') {
+          var closeFnPartial = function(tabId) { chrome.tabs.remove(tabId); }.bind(null, parseInt(components[0], 10));
+        // Close the tab once the download is over.
+          chrome.downloads.download({url: components[1]}, closeFnPartial);
+        }
+      }
+    });
+  }
 }
