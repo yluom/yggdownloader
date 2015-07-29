57d7834dea45488b294846d34afad95c2a64eb19
Manu Cornet <manu@manucornet.net>
2015-07-28 20:10:26 -0700

    Remove irrelevant comment.

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var t411Downloader = new T411Downloader();
 
-// Set up a click handler so that we can tile all the windows.
 chrome.browserAction.onClicked.addListener(t411Downloader.start.bind(t411Downloader));
 
